67e0ba2b76a8a71356c95c3bf4bd31dcad9217d6
ReduckProject <reduckproject@gmail.com>
2023-10-22 23:55:06 +0800

    windows.md

---
+++ docs/netools/_sidebar.md
@@ -0,0 +1,1 @@
+* [nginx](/netools/nginx.md)
---
+++ docs/netools/nginx-make.sh
@@ -0,0 +1,3 @@
+ cd $1
+ ./configure --sbin-path=/usr/local/nginx/nginx  --conf-path=/usr/local/nginx/nginx.conf --pid-path=/usr/local/nginx/nginx.pid --modules-path=/usr/local/nginx/modules  --with-http_realip_module --with-stream=dynamic --with-stream_ssl_module --with-stream_realip_module
+ make & make install
